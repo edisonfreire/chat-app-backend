e03a397f1b80be7a6e64a49897240e7ecf122ee7
Edison <edisonfreire14@gmail.com>
2024-10-23 19:43:03 -0400

    fix

--- server.js
+++ server.js
@@ -1,59 +1,43 @@
-const express = require('express');
-const { Server } = require('socket.io');
-const cors = require('cors');
+const express = require("express");
+const { Server } = require("socket.io");
 
 const app = express();
-const port = process.env.PORT || 8000;
-
-const allowedOrigins = [
-  'https://chat-app-pi-virid.vercel.app',
-  'http://localhost:3000',  // for local development
-];
-
-app.use(cors({
-  origin: allowedOrigins,
-  methods: ['GET', 'POST'],
-  credentials: true,
-}));
-
+const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
-  console.log(`Node JS + Socket app listening on port ${port}`);
+  console.log(`Node JS+Socket App Listening on port ${port}`);
 });
 
 const io = new Server(server, {
   cors: {
-    origin: allowedOrigins,
-    methods: ['GET', 'POST'],
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization']
+    origin: "*",
   },
 });
 
 let onlineUsers = [];
 
-io.on('connection', (socket) => {
-  socket.on('join', (userId) => {
+io.on("connection", (socket) => {
+  socket.on("join", (userId) => {
     if (!socket.rooms.has(userId)) {
       socket.join(userId);
-      if (!onlineUsers.includes(userId)) { // doesnt matter how many times refresh
+      if (!onlineUsers.includes(userId)) {
         onlineUsers.push(userId);
       }
-
-      onlineUsers.forEach((user) => {
-        io.to(user).emit('online-users-updated', onlineUsers);
-      });
     }
-  });
 
-  socket.on('send-new-message', (message) => {
-    message.chat.users.forEach((user) => {
-      io.to(user._id).emit('new-message-received', message);
+    onlineUsers.forEach((user) => {
+      io.to(user).emit("online-users-updated", onlineUsers);
     });
   });
 
-  socket.on('read-all-messages', ({chatId, users, reacByUserId }) => {
+  socket.on("send-new-message", (message) => {
+    message.chat.users.forEach((user) => {
+      io.to(user._id).emit("new-message-received", message);
+    });
+  });
+
+  socket.on("read-all-messages", ({ chatId, users, readByUserId }) => {
     users.forEach((user) => {
-      io.to(user).emit('user-read-all-chat-messages', { chatId, reacByUserId });
+      io.to(user).emit("user-read-all-chat-messages", { chatId, readByUserId });
     });
   });
 
@@ -64,17 +48,16 @@ io.on('connection', (socket) => {
     });
   });
 
-  socket.on('logout', (userId) => {
+  socket.on("logout", (userId) => {
     socket.leave(userId);
     onlineUsers = onlineUsers.filter((user) => user !== userId);
 
     onlineUsers.forEach((user) => {
-      io.to(user).emit('online-users-updated', onlineUsers);
+      io.to(user).emit("online-users-updated", onlineUsers);
     });
   });
 });
 
-app.get('/', (req, res) => {
-  return res.send('Chat App Node JS + Socket Server');
+app.get("/", (req, res) => {
+  return res.send("This is sheychat-next-udemy nodejs server");
 });
-

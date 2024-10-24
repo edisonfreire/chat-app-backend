63f236c16a229650e9c795030c27f441265bec8a
Edison <edisonfreire14@gmail.com>
2024-10-23 20:21:47 -0400

    cors issue

--- server.js
+++ server.js
@@ -10,64 +10,66 @@ const server = http.createServer(app);
 const port = process.env.PORT || 5000;
 
 const io = new Server(server, {
- cors: {
-   origin: ["https://chat-app-qknw.vercel.app", "http://localhost:3000"],
-   methods: ["GET", "POST"],
-   allowedHeaders: ["*"],
-   credentials: true
- },
- transports: ['websocket', 'polling']
+  cors: {
+    origin: ["https://chat-app-qknw.vercel.app", "http://localhost:3000"],
+    methods: ["GET", "POST"],
+    allowedHeaders: ["*"],
+    credentials: true
+  },
+  transports: ['websocket', 'polling'],
+  pingTimeout: 60000, // Increase ping timeout to 60 seconds
+  pingInterval: 25000 // Keep alive interval
 });
 
 let onlineUsers = [];
 
 io.on("connection", (socket) => {
- socket.on("join", (userId) => {
-   if (!socket.rooms.has(userId)) {
-     socket.join(userId);
-     if (!onlineUsers.includes(userId)) {
-       onlineUsers.push(userId);
-     }
-   }
+  socket.on("join", (userId) => {
+    if (!socket.rooms.has(userId)) {
+      socket.join(userId);
+      if (!onlineUsers.includes(userId)) {
+        onlineUsers.push(userId);
+      }
+    }
 
-   onlineUsers.forEach((user) => {
-     io.to(user).emit("online-users-updated", onlineUsers);
-   });
- });
+    onlineUsers.forEach((user) => {
+      io.to(user).emit("online-users-updated", onlineUsers);
+    });
+  });
 
- socket.on("send-new-message", (message) => {
-   message.chat.users.forEach((user) => {
-     io.to(user._id).emit("new-message-received", message);
-   });
- });
+  socket.on("send-new-message", (message) => {
+    message.chat.users.forEach((user) => {
+      io.to(user._id).emit("new-message-received", message);
+    });
+  });
 
- socket.on("read-all-messages", ({ chatId, users, readByUserId }) => {
-   users.forEach((user) => {
-     io.to(user).emit("user-read-all-chat-messages", { chatId, readByUserId });
-   });
- });
+  socket.on("read-all-messages", ({ chatId, users, readByUserId }) => {
+    users.forEach((user) => {
+      io.to(user).emit("user-read-all-chat-messages", { chatId, readByUserId });
+    });
+  });
 
- socket.on("typing", ({ chat, senderId, senderName }) => {
-   chat.users.forEach((user) => {
-     if (user._id !== senderId)
-       io.to(user._id).emit("typing", { chat, senderName });
-   });
- });
+  socket.on("typing", ({ chat, senderId, senderName }) => {
+    chat.users.forEach((user) => {
+      if (user._id !== senderId)
+        io.to(user._id).emit("typing", { chat, senderName });
+    });
+  });
 
- socket.on("logout", (userId) => {
-   socket.leave(userId);
-   onlineUsers = onlineUsers.filter((user) => user !== userId);
+  socket.on("logout", (userId) => {
+    socket.leave(userId);
+    onlineUsers = onlineUsers.filter((user) => user !== userId);
 
-   onlineUsers.forEach((user) => {
-     io.to(user).emit("online-users-updated", onlineUsers);
-   });
- });
+    onlineUsers.forEach((user) => {
+      io.to(user).emit("online-users-updated", onlineUsers);
+    });
+  });
 });
 
 app.get("/", (req, res) => {
- return res.send("Chat App Node JS + Socket Server");
+  return res.send("Chat App Node JS + Socket Server");
 });
 
 server.listen(port, () => {
- console.log(`Node JS + Socket App Listening on port ${port}`);
+  console.log(`Node JS + Socket App Listening on port ${port}`);
 });
